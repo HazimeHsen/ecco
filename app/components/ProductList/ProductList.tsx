0ebc1baf8cd3cc40a7ef5b4e3d115416a36598af
Hsen-123 <hazimehussein01@gmail.com>
2023-08-04 11:11:04 +0300

    first commit

--- app/components/ProductList/ProductList.tsx
+++ app/components/ProductList/ProductList.tsx
@@ -21,7 +21,7 @@ interface ProductDataProps {
 const ProductList: React.FC<ProductDataProps> = ({ productData }) => {
   return (
     <div className="flex justify-center w-full">
-      <div className="gap-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+      <div className="gap-4 w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
         {productData &&
           productData.length > 0 &&
           productData.map((data) => <ProductCard key={data.id} data={data} />)}
