ac2a1d309909b5ee21ec936aa093a25c773e89f7
Hsen-123 <hazimehussein01@gmail.com>
2023-08-25 12:42:07 +0300

    add get user api

---
+++ app/api/users/route.tsx
@@ -0,0 +1,7 @@
+import prisma from "@/app/libs/prismadb";
+import { NextResponse } from "next/server";
+
+export async function GET(req: Request) {
+  const user = await prisma.user.findMany();
+  return NextResponse.json(user);
+}
