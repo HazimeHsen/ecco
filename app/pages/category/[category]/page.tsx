c93056afa5ce5bfca7327dcdaf3ab25a5233028d
Hsen-123 <hazimehussein01@gmail.com>
2023-09-28 19:37:13 +0300

    fix the category screen

--- app/pages/category/[category]/page.tsx
+++ app/pages/category/[category]/page.tsx
@@ -6,8 +6,10 @@ import { Params } from "next/dist/shared/lib/router/utils/route-matcher";
 import React, { useState, useEffect } from "react";
 import { toast } from "react-hot-toast";
 import axios from "axios";
-import { Product } from "@prisma/client";
+import { Product, Reviews } from "@prisma/client";
 import ClientOnly from "@/app/components/ClientOnly";
+import Loader from "@/app/components/Loader/Loader";
+import ProductCard from "@/app/components/ProductCard/ProductCard";
 
 const page = ({
   params,
@@ -18,18 +20,34 @@ const page = ({
     [key: string]: string | string | undefined;
   };
 }) => {
+  const [loading, setLoading] = useState(true);
   const [products, setProducts] = useState<Product[]>([]);
+  const [reviews, setReviews] = useState<Reviews[]>([]);
 
   useEffect(() => {
+    console.log("hello");
     const getProducts = async () => {
+      if (products.length > 0) {
+        return;
+      }
       try {
-        const { data } = await axios.get("/api/product");
+        setLoading(true);
+        const [reviewsResponse, productsResponse] = await Promise.all([
+          axios.get("http://localhost:3000/api/review"),
+          axios.get("http://localhost:3000/api/product"),
+        ]);
 
-        if (data) {
-          setProducts(data);
+        if (productsResponse.data) {
+          setProducts(productsResponse.data);
         }
+        if (reviewsResponse.data) {
+          setReviews(reviewsResponse.data);
+        }
+        setLoading(false);
       } catch (error) {
-        toast.error("Somthing went Wrong.");
+        toast.error("Something went Wrong.");
+      } finally {
+        setLoading(false);
       }
     };
     getProducts();
@@ -48,14 +66,38 @@ const page = ({
 
   return (
     <ClientOnly>
-      <div>
-        <ProductList productData={entries} />
-        <PaginationControls
-          dataNb={entries.length}
-          hasNextPage={end < data.length}
-          hasPrevPage={start > 0}
+      {loading ? (
+        <Loader
+          fill="black"
+          className="!w-[35px] !h-[35px] text-black absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 "
         />
-      </div>
+      ) : (
+        <div className="flex flex-col h-full">
+          <div className="flex-1">
+            <div className="flex justify-center w-full">
+              <div className="gap-4 w-full grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+                {products &&
+                  entries.length > 0 &&
+                  entries.map((data) => (
+                    <ProductCard
+                      reviews={reviews}
+                      setLoading={setLoading}
+                      key={data.id}
+                      data={data}
+                    />
+                  ))}
+              </div>
+            </div>
+          </div>
+          <div>
+            <PaginationControls
+              dataNb={products.length}
+              hasNextPage={end < products.length}
+              hasPrevPage={start > 0}
+            />
+          </div>
+        </div>
+      )}
     </ClientOnly>
   );
 };
